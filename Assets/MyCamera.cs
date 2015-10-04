b12d23d598d0414319db0323d7308424f92d2fc4
GintaBerry_Windows <ntgbn@qq.com>
2015-10-04 13:10:19 +0800

    with light and movement

---
+++ Assets/MyCamera.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyCamera : MonoBehaviour {
+
+    GameObject player;
+    Transform playerTransform;
+    private Vector3 velocity = Vector3.zero;
+    Camera myCamera;
+    public float dampTime = 0.15f;
+	// Use this for initialization
+	void Start () {
+        myCamera = GetComponent<Camera>();
+        player = GameObject.Find("Cube");
+        playerTransform = player.GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(playerTransform)
+        {
+            Vector3 point = myCamera.WorldToViewportPoint(playerTransform.position);
+            Vector3 delta = playerTransform.position - myCamera.ViewportToWorldPoint(new Vector3(0.5f,  0.5f, point.z));
+            Vector3 destination = transform.position + delta;
+            Debug.Log(transform.position);
+            transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+        }
+	}
+}
---
+++ Assets/MyPlayerControl.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyPlayerControl : MonoBehaviour
+{
+
+    float maxSpeed = 1;
+    float moveForce = 10;
+    float speed = 5;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("Current Speed" + this.GetComponent<Rigidbody>().velocity);
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(h * Time.deltaTime, 0, v * Time.deltaTime) * speed, Space.World);
+        //if (this.GetComponent<Rigidbody>().velocity.magnitude > 0.5)
+        transform.LookAt(transform.position + new Vector3(h, 0, v));
+        #region question
+        //How to translate it using force
+        //GetComponent<Rigidbody>().AddForce((Vector3.right * h + Vector3.forward * v) * moveForce);
+        #endregion
+    }
+}
